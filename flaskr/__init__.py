37ec5ef3551f3bcb9b106ef20c5be0fecef3620b
Conor Wallwork <conorw@ngis.com.au>
2023-06-02 22:27:47 +0800

    changed config load method

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,21 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        MYSQL_USER="ConorWallwork",
-        MYSQL_PASSWORD="Conor123!",
-        MYSQL_HOST="127.0.0.1",
-        MYSQL_DATABASE="blackjack",
-        STARTING_STACK=200
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    app.config.from_pyfile('config.py', silent=True)
 
     # ensure the instance folder exists
     try:
@@ -30,12 +16,11 @@ def create_app(test_config=None):
 
     from .controller import seat
     app.register_blueprint(seat.bp, url_prefix="/seat")
-    
-        # a simple page that says hello
+
+    # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-
 
     from . import db
     db.init_app(app)
